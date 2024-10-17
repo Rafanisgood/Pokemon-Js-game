788b861905e134e36f8eb2a8c8e42ea2baa836ab
Arthur <150600141+Rafanisgood@users.noreply.github.com>
2024-10-17 12:55:15 +0600

    rendered map and added character

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 const canvas=document.querySelector('canvas');
-const c = canvas.getContext()
+const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
-console.log(canvas)
+
+c.fillStyle = 'white'
+c.fillRect
+(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.src='./images/whale village.png'
+
+const playerImage = new Image()
+playerImage.src = 
+'images/playerDown.png'
+
+image.onload = () => {
+    c.drawImage(
+        image, -510, -640 
+       )
+    c.drawImage(
+        playerImage,
+         0, 0,
+        playerImage.width / 4,
+        playerImage.height,
+        canvas.width / 2 - playerImage.width / -3,
+        canvas.height / 2 - playerImage.height / -3, playerImage.width / 4, playerImage.height
+       )
+}
+
+window.addEventListener('keydown', (e) =>{
+    console.log('go')
+})
