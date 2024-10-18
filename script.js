626c8346371fb9186e9d780bb0757b490a832ffe
Arthur <150600141+Rafanisgood@users.noreply.github.com>
2024-10-19 03:22:17 +0600

    solved the character loading issue
    the character was being drawn before the character image was done being loaded

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-const canvas=document.querySelector('canvas');
-const c = canvas.getContext('2d')
-canvas.width = 1024
-canvas.height = 576
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+canvas.width = 1024;
+canvas.height = 576;
 
-c.fillStyle = 'white'
-c.fillRect
-(0, 0, canvas.width, canvas.height)
+c.fillStyle = 'white';
+c.fillRect(0, 0, canvas.width, canvas.height);
 
-const image = new Image()
-image.src='./images/whale village.png'
+const image = new Image();
+image.src = './images/whale village.png';
 
-const playerImage = new Image()
-playerImage.src = 
-'images/playerDown.png'
+const playerImage = new Image();
+playerImage.src = 'images/playerDown.png';
 
-image.onload = () => {
-    c.drawImage(
-        image, -510, -640 
-       )
-    c.drawImage(
-        playerImage,
-         0, 0,
-        playerImage.width / 4,
-        playerImage.height,
-        canvas.width / 2 - playerImage.width / -3,
-        canvas.height / 2 - playerImage.height / -3, playerImage.width / 4, playerImage.height
-       )
+function draw() {
+    if (image.complete) {
+        c.drawImage(image, -510, -640);
+    }
+
+    if (playerImage.complete) {
+        c.drawImage(
+            playerImage,
+            0, 0,
+            playerImage.width / 4,
+            playerImage.height,
+            canvas.width / 2 - playerImage.width / 4 / 2,
+            canvas.height / 2 - playerImage.height / 2,
+            playerImage.width / 4,
+            playerImage.height
+        ); 
+    }
 }
 
-window.addEventListener('keydown', (e) =>{
-    console.log('go')
-})
+image.onload = draw;
+playerImage.onload = draw;
+
+window.addEventListener('keydown', (e) => {
+    console.log('go');
+});
